9dbf354e5623f00cc0b4a1bc01e789f62581aa5b
Mohamed-Ghaith Kaabi <mgk4dev@gmail.com>
2024-09-25 19:16:10 +0200

    Add Flags

---
+++ src/libs/core/headers/ez/Flags.hpp
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <ez/Utils.hpp>
+
+namespace ez {
+template <typename Enum>
+class Flags {
+public:
+    using UnderlyingInt = std::underlying_type_t<Enum>;
+
+    constexpr Flags(Enum val) noexcept : m_value(ez::as_int(val)) {}
+
+    constexpr Flags(UnderlyingInt val) noexcept : m_value(val) {}
+
+    Flags() = default;
+    Flags(const Flags&) = default;
+    Flags(Flags&&) = default;
+    Flags& operator=(const Flags&) = default;
+    Flags& operator=(Flags&&) = default;
+
+public:
+    constexpr Flags operator|(Flags another) const noexcept
+    {
+        return Flags(m_value | another.m_value);
+    }
+    constexpr Flags operator^(Flags another) const noexcept
+    {
+        return Flags(m_value ^ another.m_value);
+    }
+    constexpr Flags operator|=(Flags another) noexcept
+    {
+        m_value |= another.m_value;
+        return m_value;
+    }
+    constexpr Flags operator^=(Flags another) noexcept
+    {
+        m_value ^= another.m_value;
+        return m_value;
+    }
+
+    template <typename T>
+    friend constexpr bool operator==(const Flags<T>& a, const Flags<T>& b) noexcept;
+
+    template <typename T>
+    friend constexpr bool operator!=(const Flags<T>& a, const Flags<T>& b) noexcept;
+
+    constexpr bool test(Enum flag) const noexcept { return (m_value & ez::as_int(flag)) == ez::as_int(flag); }
+    template <typename... Values>
+    constexpr bool all(Values... values) const noexcept
+    {
+        return (test(values) && ...);
+    }
+    template <typename... Values>
+    constexpr bool any(Values... values) const noexcept
+    {
+        return (test(values) || ...);
+    }
+    constexpr explicit operator bool() const noexcept { return bool(m_value); }
+    constexpr UnderlyingInt value() const noexcept { return m_value; }
+    constexpr Flags& set(Enum flag, bool on = true) noexcept
+    {
+        if (on) { m_value |= ez::as_int(flag); }
+        else {
+            m_value &= ~ez::as_int(flag);
+        }
+        return *this;
+    }
+    constexpr void clear() noexcept { m_value = 0; }
+    constexpr void clear(Enum flag) noexcept { m_value &= ~ez::as_int(flag); }
+
+private:
+    UnderlyingInt m_value{0};
+};
+
+template <typename T>
+constexpr bool operator==(const Flags<T>& a, const Flags<T>& b) noexcept
+{
+    return a.m_value == b.m_value;
+}
+
+template <typename T>
+constexpr bool operator!=(const Flags<T>& a, const Flags<T>& b) noexcept
+{
+    return !(a == b);
+}
+
+template <typename Enum>
+constexpr Flags<Enum> make_flags(Enum val) noexcept
+{
+    return Flags<Enum>(val);
+}
+
+template <typename Enum, typename... Args>
+constexpr Flags<Enum> make_flags(Enum first, Args... args) noexcept
+{
+    return Flags<Enum>(first) | make_flags(args...);
+}
+
+}  // namespace ez
---
+++ src/libs/core/tests/tst_Flags.cpp
@@ -0,0 +1,138 @@
+#include <gtest/gtest.h>
+
+#include <ez/Flags.hpp>
+
+using namespace ez;
+
+enum MyFlags
+{
+    NoOption,
+    Option1 = 1,
+    Option2 = 2,
+    Option3 = 4
+};
+
+TEST(Flags, make_flags_regular_enum)
+{
+    make_flags(Option1);
+    SUCCEED();
+}
+
+TEST(Flags, test_regular_enum)
+{
+    auto f = make_flags(Option1);
+    ASSERT_TRUE(f.test(Option1));
+    ASSERT_FALSE(f.test(Option2));
+}
+
+TEST(Flags, set_regular_enum)
+{
+    Flags<MyFlags> f;
+    f.set(Option1, true);
+    ASSERT_TRUE(f.test(Option1));
+    ASSERT_FALSE(f.test(Option2));
+
+    f.set(Option1, false);
+    ASSERT_FALSE(f.test(Option1));
+}
+
+TEST(Flags, or_regular_enum)
+{
+    Flags<MyFlags> f = Option1 | Option2;
+    ASSERT_TRUE(f.test(Option1));
+    ASSERT_TRUE(f.test(Option2));
+    ASSERT_FALSE(f.test(Option3));
+
+    Flags<MyFlags> f1 = Option1;
+    Flags<MyFlags> f2 = Option2;
+    f = f1 | f2;
+    ASSERT_TRUE(f.test(Option1));
+    ASSERT_TRUE(f.test(Option2));
+    ASSERT_FALSE(f.test(Option3));
+}
+
+enum class Options
+{
+    NoOption,
+    Option1 = 1,
+    Option2 = 2,
+    Option3 = 4
+};
+
+TEST(Flags, make_flags_class_enum)
+{
+    make_flags(Options::Option1);
+    SUCCEED();
+}
+
+TEST(Flags, test_class_enum)
+{
+    auto f = make_flags(Options::Option1);
+    ASSERT_TRUE(f.test(Options::Option1));
+    ASSERT_FALSE(f.test(Options::Option2));
+}
+
+TEST(Flags, test_multiple_class_enum)
+{
+    auto f = make_flags(Options::Option1, Options::Option2);
+    ASSERT_TRUE(f.all(Options::Option1, Options::Option2));
+    ASSERT_FALSE(f.all(Options::Option1, Options::Option3));
+}
+
+TEST(Flags, boolean_operator_class_enum)
+{
+    auto f = make_flags(Options::Option1);
+    ASSERT_TRUE(bool(f));
+    ASSERT_FALSE(bool(Flags<Options>{}));
+}
+
+TEST(Flags, any_class_enum)
+{
+    auto f = make_flags(Options::Option1, Options::Option2);
+    ASSERT_TRUE(f.any(Options::Option1, Options::Option3));
+    ASSERT_TRUE(f.any(Options::Option2, Options::Option3));
+    ASSERT_FALSE(f.any(Options::Option3));
+}
+
+TEST(Flags, set_class_enum)
+{
+    Flags<Options> f;
+    f.set(Options::Option1, true);
+    ASSERT_TRUE(f.test(Options::Option1));
+    ASSERT_FALSE(f.test(Options::Option2));
+
+    f.set(Options::Option1, false);
+    ASSERT_FALSE(f.test(Options::Option1));
+}
+
+TEST(Flags, or_class_enum)
+{
+    Flags<Options> f = as_int(Options::Option1) | as_int(Options::Option2);
+    ASSERT_TRUE(f.test(Options::Option1));
+    ASSERT_TRUE(f.test(Options::Option2));
+    ASSERT_FALSE(f.test(Options::Option3));
+
+    Flags<Options> f1 = Options::Option1;
+    Flags<Options> f2 = Options::Option2;
+    f = f1 | f2;
+    ASSERT_TRUE(f.test(Options::Option1));
+    ASSERT_TRUE(f.test(Options::Option2));
+    ASSERT_FALSE(f.test(Options::Option3));
+}
+
+TEST(Flags, equality_operator_class_enum)
+{
+    Flags<Options> f1 = Options::Option1;
+    Flags<Options> f2 = Options::Option1;
+    ASSERT_TRUE(f1 == f2);
+
+    f1 = f1 | as_int(Options::Option2);
+    ASSERT_FALSE(f1 == f2);
+    ASSERT_TRUE(f1 != f2);
+
+    f2 = f2 | as_int(Options::Option2);
+    ASSERT_TRUE(f1 == f2);
+
+    f1 = Flags<Options>();
+    ASSERT_FALSE(f1 == f2);
+}
